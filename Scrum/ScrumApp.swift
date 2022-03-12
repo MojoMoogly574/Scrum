a2a5de863a7582c945033ed9348554921eabf655
Mojo <mojo@nuralab.com>
2022-03-12 16:23:22 -0600

    Initial Commit

---
+++ Scrum/ScrumApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumApp.swift
+//  Scrum
+//
+//  Created by Joseph DeWeese on 3/12/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
